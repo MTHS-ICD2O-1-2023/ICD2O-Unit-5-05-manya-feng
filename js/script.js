959bf81cfdac6cb74c8f4a4db4e255f04b82385e
Manya Feng <159185413+manya-feng@users.noreply.github.com>
2024-05-05 20:59:50 +0000

    change

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 // This file contains the JS functions for index.html
 "use strict"
 
-function calculate() {
+function button() {
 
   const lengthAString = parseInt(document.getElementById("lengthA").value)
   const lengthBString = parseInt(document.getElementById("lengthB").value)
@@ -19,13 +19,6 @@ function calculate() {
   const angleA = Math.acos((lengthB ** 2 + lengthC ** 2 - lengthA ** 2) / (2 * lengthB * lengthC)) * (180 / Math.PI)
   const angleB = Math.acos((lengthC ** 2 + lengthA ** 2 - lengthB ** 2) / (2 * lengthC * lengthA)) * (180 / Math.PI)
   const angleC = Math.acos((lengthA ** 2 + lengthB ** 2 - lengthC ** 2) / (2 * lengthA * lengthB)) * (180 / Math.PI)
-
-  const sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + Number((angleC).toFixed(2))
-
-  console.log(angleA)
-  console.log(angleB)
-  console.log(angleC)
-  console.log(sumOfAngles)
 
   if (angleA == angleB && angleA == angleC) {
     document.getElementById("math").innerHTML =
